e96df519e92e526cc92c4cf88b380df7c2f40629
likun <1617455243@qq.com>
2019-03-31 19:21:10 +0800

    https

--- ghost-config.js
+++ ghost-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "host": "http://blog.likun.fun",
+  "host": "https://blog.likun.fun",
   "client_id": "ghost-frontend",
   "client_secret": "00585fe4617a",
   "url_prefix": "/ghost/api/v0.1"
